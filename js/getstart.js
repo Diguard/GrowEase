e9f1d95c5f5c44206321abdde4b729510fb897e7
Diguard <fsdevdiguard@gmail.com>
2025-12-02 13:32:04 +0200

    Update for button send

---
+++ js/getstart.js
@@ -0,0 +1,7 @@
+const buttonRef = document.querySelector(".form__button");
+
+const handBtnClick = function () {
+  alert("Our manager will contact you shortly. Thank you for choosing us.");
+};
+
+buttonRef.addEventListener("click", handBtnClick);

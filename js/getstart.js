60c5b1e07f206d515d8899591724d22641057cce
Diguard <fsdevdiguard@gmail.com>
2026-02-20 00:45:47 +0200

    refactor: remove unused script and improve form submission

--- js/getstart.js
+++
@@ -1,7 +0,0 @@
-const buttonRef = document.querySelector(".form__button");
-
-const handBtnClick = function () {
-  alert("Our manager will contact you shortly. Thank you for choosing us.");
-};
-
-buttonRef.addEventListener("click", handBtnClick);
--- js/modal.js
+++ js/modal.js
@@ -1,16 +1,47 @@
+// (() => {
+//   const refs = {
+//     openModalBtn: document.querySelector('[data-modal-open]'),
+//     closeModalBtn: document.querySelector('[data-modal-close]'),
+//     modal: document.querySelector('[data-modal]'),
+//     body: document.querySelector('[data-body]'),
+//   };
+
+//   refs.openModalBtn.addEventListener('click', toggleModal);
+//   refs.closeModalBtn.addEventListener('click', toggleModal);
+
+//   function toggleModal() {
+//     refs.modal.classList.toggle('is-hidden');
+//     refs.body.classList.toggle('no-scroll');
+//   }
+// })();
+
 (() => {
   const refs = {
-    openModalBtn: document.querySelector("[data-modal-open]"),
-    closeModalBtn: document.querySelector("[data-modal-close]"),
-    modal: document.querySelector("[data-modal]"),
-    body: document.querySelector("[data-body]"),
+    openModalBtn: document.querySelector('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
   };
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
-
-  function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
-    refs.body.classList.toggle("no-scroll");
-  }
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
 })();
+
+function toggleModal() {
+  const modal = document.querySelector('[data-modal]');
+  const body = document.querySelector('[data-body]');
+
+  modal.classList.toggle('is-hidden');
+  body.classList.toggle('no-scroll');
+}
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  alert('Our manager will contact you shortly. Thank you for choosing us.');
+
+  toggleModal();
+  form.reset();
+}
